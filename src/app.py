aeacf2d62df7c9fc5f7726677b510d9ebe56c4f8
labs404 <136141267+labs404@users.noreply.github.com>
2023-09-12 01:01:25 +0000

    completed delete functionality

--- src/app.py
+++ src/app.py
@@ -15,6 +15,29 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+# create dictionaries to add to the jackson family list
+# create 3 dictionaries of the family members in the instructions
+jackson_family.add_member({ 
+    "first_name": "John",
+    "last_name": "Jackson",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+ })
+
+jackson_family.add_member({ 
+    "first_name": "Jane",
+    "last_name": "Jackson",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+ })
+
+jackson_family.add_member({ 
+    "first_name": "Jimmy",
+    "last_name": "Jackson",
+    "age": 5,
+    "lucky_numbers": [1]
+ })
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,17 +49,27 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def get_all_family_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route("/member/<int:member_id>", methods=["GET"])
+def get_single_family_member(member_id):
+    member = jackson_family.get_member(member_id)
+    return jsonify(member), 200
 
-    return jsonify(response_body), 200
+@app.route("/member", methods=["POST"])
+def create_new_member():
+    member = request.json
+    jackson_family.add_member(member)
+    return f"family member successfully added: {member['first_name']} {member['last_name']}", 200
+
+@app.route("/delete/<int:member_id>", methods=["DELETE"])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    return f"successfully deleted {member_id}"
+    # return jsonify(member), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -19,18 +19,25 @@ class FamilyStructure:
     def _generateId(self):
         return randint(0, 99999999)
 
-    def add_member(self, member):
-        # fill this method and update the return
-        pass
-
-    def delete_member(self, id):
-        # fill this method and update the return
-        pass
-
-    def get_member(self, id):
-        # fill this method and update the return
-        pass
-
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+    
+
+    def get_member(self, id):
+        for member in self._members:
+            if member['id'] == int(id):
+                return member
+        return None
+    
+    def add_member(self, member):
+        id = self._generateId()
+        member['id'] = id
+        self._members.append(member)
+        return None
+
+    def delete_member(self, id):
+        for member in self._members:
+            if member['id'] == int(id):
+                self._members.remove(member)
+        return None
